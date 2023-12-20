062eae5d9d5fade04fdff2422f3777c8e6627f79
Saurabh <saurabhs26667@gmail.com>
2023-12-20 11:35:43 +0530

    Setting up inefra app

--- App.js
+++ App.js
@@ -1,7 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
 
 export default function App() {
+  const [theme, setTheme] = useState(ConfigApp.THEMEMODE);
+  const [isLogged, setIsLogged] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [language, setLanguage] = useState(ConfigApp.DEFAULTLANG);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+    AsyncStorage.setItem("themeSetting", theme);
+  };
+
+  const preference = useMemo(
+    () => ({
+      toggleTheme,
+      theme,
+    }),
+    [theme]
+  );
+
+  const updateValue = (lang) => {
+    setLanguage(lang);
+    AsyncStorage.setItem("language", lang);
+  };
+
+  useEffect(() => {
+    async function checkUser() {
+      onAuthStateChanged(auth, (user) => {
+        if (user !== null) {
+          setIsLogged(true);
+          setLoaded(true);
+        } else {
+          setIsLogged(false);
+          setLoaded(true);
+        }
+      });
+    }
+
+    checkUser();
+    console.log("isLogged", isLogged);
+  }, []);
+
+  useEffect(() => {
+    async function checkTheme() {
+      await AsyncStorage.getItem("themeSetting").then((value) => {
+        if (value) {
+          setTheme(value === "dark" ? "light" : "dark");
+        }
+      });
+    }
+
+    checkTheme();
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
